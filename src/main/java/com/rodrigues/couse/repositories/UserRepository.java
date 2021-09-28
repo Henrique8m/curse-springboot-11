a7147282bf9e2b573a034b32c10c43d89a07ce3c
Henrique Rodrigues <hyck.rocha@gmail.com>
2021-09-28 12:10:54 -0300

    Service layer, component registration

--- src/main/java/com/rodrigues/couse/repositories/UserRepository.java
+++ src/main/java/com/rodrigues/couse/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.rodrigues.couse.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rodrigues.couse.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
