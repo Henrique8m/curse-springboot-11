1023f716025024cb09510b21f892db2b929de681
Henrique Rodrigues <hyck.rocha@gmail.com>
2021-12-27 08:11:17 -0300

    Exception handling - update

--- src/main/java/com/rodrigues/couse/services/UserService.java
+++ src/main/java/com/rodrigues/couse/services/UserService.java
@@ -3,6 +3,8 @@ package com.rodrigues.couse.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -18,49 +20,57 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
-		
+
+	public List<User> findAll() {
+
 		List<User> list = userRepository.findAll();
 		return list;
-		
+
 	}
-	
+
 	public User findById(Long id) {
-		
+
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-		
+
 	}
-	
+
 	public User insert(User obj) {
-		
+
 		return userRepository.save(obj);
-		
+
 	}
-	
+
 	public void delete(Long id) {
 		try {
-			
+
 			userRepository.deleteById(id);
-		
-		}catch (EmptyResultDataAccessException e) {
+
+		} catch (EmptyResultDataAccessException e) {
 
 			throw new ResourceNotFoundException(id);
-			
-		}catch (DataIntegrityViolationException e) {
-			
+
+		} catch (DataIntegrityViolationException e) {
+
 			throw new DatabaseException(e.getMessage());
-			
+
 		}
-			
+
 	}
-	
+
 	public User update(Long id, User obj) {
 		
-		User entity = userRepository.getOne(id); //get one monitora o objeto a ser alterado, e nao o tras como o getid
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			
+			User entity = userRepository.getOne(id); //get one monitora o objeto a ser alterado, e nao o tras como o getid
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		
+		}catch (EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException(id);
+			
+		}
 		
 	}
 
@@ -68,7 +78,7 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
+
 }
